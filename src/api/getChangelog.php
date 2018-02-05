982e9cc13801335a65fcd2a925a32dc1e7caf706
Isaac Hookom <isaac.hookom@gmail.com>
2018-02-05 13:47:15 -0500

    Update getChangelog.php

--- src/api/getChangelog.php
+++ src/api/getChangelog.php
@@ -2,6 +2,6 @@
     require_once('db.php');
 
     $db = new Db();
-    $rows = $db -> select("SELECT * FROM changelog ORDER BY timestamp DESC");
+    $rows = $db -> select("SELECT * FROM changelog ORDER BY timestamp DESC LIMIT 20");
     echo json_encode($rows);
 ?>
