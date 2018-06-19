dcffb5de7a3ce8f33f6e263d282919479260e990
Isaac Hookom <isaac.hookom@gmail.com>
2018-06-18 22:38:56 -0400

    db convert scripts

--- api/changelog_insert.js
+++ api/changelog_insert.js
@@ -1,7 +1,17 @@
 const Datastore = require('@google-cloud/datastore');
+const fs = require('fs');
 
 let datastore = new Datastore({
     projectId: 'undead-darts-1'
 });
+
+var lines = fs.readFileSync('changelog','utf8').split('\n');
+
+lines.forEach(row => {
+    let cols = row.split(' ');
+    let timestamp = cols[5] + ' ' + cols[6];
+    timestamp = timestamp.substr(1, timestamp.length-4);
+    let data = { message: cols[4].substr(2, cols[4].length-4), timestamp }
+    datastore.insert({ key: datastore.key('ChangelogTest'), data });
+})
   
-datastore.insert({message: 'test', timestamp: '2018-01-26 13:57:30'});
